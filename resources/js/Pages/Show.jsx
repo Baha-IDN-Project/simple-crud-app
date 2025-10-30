7e90bf5bf0568b3d98da9356160b5d008114c635
bahamud11 <ahmadbahaudinmudhari@gmail.com>
2025-10-30 20:20:44 +0700

    first commit

---
+++ resources/js/Pages/Show.jsx
@@ -0,0 +1,36 @@
+import { Link, useForm } from "@inertiajs/react";
+import { useRoute } from "../../../vendor/tightenco/ziggy"
+
+export default function Show({post}) {
+    const { delete: destroy } = useForm();
+    const route = useRoute();
+
+    function submit(e) {
+        e.preventDefault()
+        // destroy(`/posts/${post.id}`)
+        destroy(route("posts.destroy", post))
+    }
+
+    return (
+        <>
+            <div className="p-4 border-b">
+                <div className="text-sm text-slate-600">
+                    <span>Posted on: </span>
+                    <span>{ new Date(post.created_at).toLocaleDateString() }</span>
+                </div>
+                <p className="font-medium">{post.body}</p>
+
+                <div className="flex items-center justify-end gap-2">
+                    <form onSubmit={submit}>
+                        <button className="bg-red-500 rounded-md text-sm px-4 py-1 text-white">
+                            Delete
+                        </button>
+                    </form>
+                    <Link href={`/posts/${post.id}/edit`} className="bg-blue-500 rounded-md text-sm px-4 py-1 text-white">
+                    Update
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
